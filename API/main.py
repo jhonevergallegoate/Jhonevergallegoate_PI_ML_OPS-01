402740fabb2fba7d31a22d3b04affbdfe01dd374
JEGA <jhgallegoa21@gmail.com>
2023-10-31 23:30:02 -0500

    Seguimos

--- API/main.py
+++ API/main.py
@@ -14,9 +14,9 @@ def Hello():
     return 'Author: Jhon Ever Gallego'
 
 # Creamos la ruta de los datos.
-df_games_items_path = 'C:/Users/jhgal/Escritorio/Jhonevergallegoate_PI_ML_OPS-01/API/DATA/df_games_items.csv'
-df_games_path = 'C:/Users/jhgal/Escritorio/Jhonevergallegoate_PI_ML_OPS-01/API/DATA/df_games.csv'
-df_steam_path = 'C:/Users/jhgal/Escritorio/Jhonevergallegoate_PI_ML_OPS-01/API/DATA/df_steam.csv.gz'
+df_games_items_path = 'API/df_games_items.csv'
+df_games_path = 'API\df_games.csv'
+df_steam_path = 'API/df_steam.csv.gz'
 
 # Cargamos los datos.
 df_games_items = pd.read_csv(df_games_items_path)
