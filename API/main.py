a3b1d31196ccd1b438d76faf242b74b1d86b181e
JEGA <jhgallegoa21@gmail.com>
2023-11-01 21:56:55 -0500

    ETL completo

--- API/main.py
+++ API/main.py
@@ -1,3 +1,4 @@
+# Importamos las librerías necesarias.
 import pandas as pd
 import numpy as np
 import gzip
@@ -8,7 +9,7 @@ from fastapi import HTTPException
 
 app = FastAPI()
 
-# Raiz
+# Raiz.
 @app.get("/")
 def Hello():
     return 'Author: Jhon Ever Gallego'
@@ -28,12 +29,14 @@ Función 1:  def PlayTimeGenre(genero : str): Debe devolver año con mas horas j
                     Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
 '''
 
-# http://localhost:8000/PlayTimeGenre/action
+# LOCAL HOST: http://localhost:8000/PlayTimeGenre/action
+# REQUEST URL: https://jhonevergallegoate-pi-ml-ops-01.onrender.com/PlayTimeGenre/action
 @app.get("/PlayTimeGenre/{genero}")
 def PlayTimeGenre(genero: str):
     try:
         # Filtrar el DataFrame por el género especificado
         df_genre = df_games_items[df_games_items['genres'].str.contains(genero, case=False)]  # Usar case=False para hacerlo insensible a mayúsculas/minúsculas
+
         if df_genre.empty:
             return {"message": f"No hay datos de horas jugadas para el género {genero}"}
         # Hacer una copia del DataFrame filtrado
@@ -44,11 +47,14 @@ def PlayTimeGenre(genero: str):
         df_genre['release_year'] = df_genre['release_date'].dt.year
         # Agrupar por año, calcular las horas jugadas totales
         result = df_genre.groupby(df_genre['release_year'])['playtime_forever'].sum().reset_index()
+
         if result.empty:
             return {"message": f"No hay datos de horas jugadas para el género {genero}"}
         # Encontrar el año con más horas jugadas para el género
         max_year = result.loc[result['playtime_forever'].idxmax()]
+
         return {"Año de lanzamiento con más horas jugadas para Género " + genero: int(max_year['release_year'])}
+
     except Exception as e:
         return {"error": str(e)}
 
@@ -59,7 +65,8 @@ Función 2:  def UserForGenre( genero : str ): Debe devolver el usuario que acum
                         "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
 '''
 
-# http://localhost:8000/UserForGenre/action
+# LOCAL HOST: http://localhost:8000/UserForGenre/action
+# REQUEST URL: https://jhonevergallegoate-pi-ml-ops-01.onrender.com/UserForGenre/action
 @app.get("/UserForGenre/{genero}")
 def UserForGenre(genero: str):
     try:
@@ -70,6 +77,7 @@ def UserForGenre(genero: str):
                 f"Usuario con más horas jugadas para Género {genero}": None,
                 "Horas jugadas": []
             }
+
         # Hacer una copia del DataFrame filtrado
         df_genre = df_genre.copy()
         # Convierte la columna 'release_date' en tipo datetime
@@ -78,11 +86,13 @@ def UserForGenre(genero: str):
         df_genre['release_year'] = df_genre['release_date'].dt.year
         # Agrupar por usuario y año, calcular las horas jugadas totales
         result = df_genre.groupby(['user_id', df_genre['release_year']])['playtime_forever'].sum().reset_index()
+
         if result.empty:
             return {
                 f"No hay datos de horas jugadas para el género {genero}": None,
                 "Horas jugadas": []
             }
+
         # Encontrar al usuario con más horas jugadas para el género
         max_user = result.loc[result.groupby('user_id')['playtime_forever'].idxmax()]
         max_user_id = max_user['user_id'].values[0]
@@ -90,10 +100,12 @@ def UserForGenre(genero: str):
         total_hours_by_year = result.groupby('release_year')['playtime_forever'].sum().reset_index()
         # Crear la lista de acumulación de horas jugadas por año en el formato deseado
         horas_por_ano = [{"Año": int(row['release_year']), "Horas": int(row['playtime_forever'])} for _, row in total_hours_by_year.iterrows()]
+
         return {
             f"Usuario con más horas jugadas para Género {genero}": max_user_id,
             "Horas jugadas": horas_por_ano
         }
+
     except Exception as e:
         return {"error": str(e)}  # Devolvemos un mensaje de error en caso de excepción
 
@@ -101,10 +113,10 @@ def UserForGenre(genero: str):
 Función 3:  def UsersRecommend( año : int ): Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. 
                 (reviews.recommend = True y comentarios positivos/neutrales)
             Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
-                        
 '''
 
-# http://localhost:8000/UsersRecommend/2011
+# LOCAL HOST: http://localhost:8000/UsersRecommend/2011
+# REQUEST URL: https://jhonevergallegoate-pi-ml-ops-01.onrender.com/UsersRecommend/2011
 @app.get("/UsersRecommend/{year}")
 def UsersRecommend(year: int):
     try:
@@ -119,11 +131,15 @@ def UsersRecommend(year: int):
         # Ordenamos de mayor a menor
         data_year = data_year.sort_values(by="recommend", ascending=False)
         # Obtenemos el top 3
-        top3 = data_year.head(3).to_dict("records")
-        return {
-            "Top 3 de juegos MÁS recomendados por usuarios para el año": year,
-            "Top 3": top3,  # Asegúrate de definir top3 adecuadamente
-        }
+        top3 = data_year.head(3)
+
+        # Formateamos el resultado en el formato deseado
+        result = [{"Puesto 1": top3.iloc[0]["app_name"]},
+                {"Puesto 2": top3.iloc[1]["app_name"]},
+                {"Puesto 3": top3.iloc[2]["app_name"]}]
+
+        return result
+
     except KeyError:
         raise HTTPException(status_code=404, detail="No se encontraron datos para el año especificado.")
     except Exception as e:
@@ -135,7 +151,8 @@ Función 4:  def UsersNotRecommend( año : int ): Devuelve el top 3 de juegos ME
             Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
 '''
 
-# http://localhost:8000/UsersNotRecommend/2009
+# LOCAL HOST: http://localhost:8000/UsersNotRecommend/2009
+# REQUEST URL: https://jhonevergallegoate-pi-ml-ops-01.onrender.com/UsersNotRecommend/2009
 @app.get("/UsersNotRecommend/{year}")
 def UsersNotRecommend(year: int):
     try:
@@ -150,11 +167,15 @@ def UsersNotRecommend(year: int):
         # Ordenamos de mayor a menor
         data_year = data_year.sort_values(by="recommend", ascending=False)
         # Obtenemos el top 3
-        top3 = data_year.head(3).to_dict("records")
-        return {
-            "Top 3 de juegos MENOS recomendados por usuarios para el año": year,
-            "Top 3": top3,  # Asegúrate de definir top3 adecuadamente
-        }
+        top3 = data_year.head(3)
+
+        # Formateamos el resultado en el formato deseado
+        result = [{"Puesto 1": top3.iloc[0]["app_name"]},
+                {"Puesto 2": top3.iloc[1]["app_name"]},
+                {"Puesto 3": top3.iloc[2]["app_name"]}]
+
+        return result
+
     except KeyError:
         raise HTTPException(status_code=404, detail="No se encontraron datos para el año especificado.")
     except Exception as e:
@@ -166,7 +187,8 @@ Función 5:  def sentiment_analysis( año : int ): Según el año de lanzamiento
             Ejemplo de retorno: {Negative = 182, Neutral = 120, Positive = 278}
 '''
 
-# http://localhost:8000/sentiment_analysis/2010
+# LOCAL HOST: http://localhost:8000/sentiment_analysis/2010
+# REQUEST URL: https://jhonevergallegoate-pi-ml-ops-01.onrender.com/sentiment_analysis/2010
 @app.get("/sentiment_analysis/{year}")
 def sentiment_analysis(year: int):
     try:
@@ -177,27 +199,33 @@ def sentiment_analysis(year: int):
         # Verificamos que se obtuvieron datos para el año especificado
         if data_year.empty:
             raise HTTPException(status_code=404, detail=f"No se encontraron datos para el año {year}.")
-        # Obtenemos el top 3
-        sentiment = data_year.to_dict("records")
+        
         # Inicializar contadores
         negative_count = 0
         neutral_count = 0
         positive_count = 0
+
         # Contar el número de reseñas con cada sentimiento
-        for s in sentiment:
-            if s["sentiment"] == 0:
-                negative_count += s["review"]
-            elif s["sentiment"] == 1:
-                neutral_count += s["review"]
-            elif s["sentiment"] == 2:
-                positive_count += s["review"]
+        for index, row in data_year.iterrows():
+            sentiment = row["sentiment"]
+            review_count = row["review"]
+
+            if sentiment == 0:
+                negative_count += review_count
+            elif sentiment == 1:
+                neutral_count += review_count
+            elif sentiment == 2:
+                positive_count += review_count
+
         # Crear el diccionario con los contadores
-        sentiment = {
+        sentiment_counts = {
             "Negative": negative_count,
             "Neutral": neutral_count,
             "Positive": positive_count,
         }
-        return {"Según el año de lanzamiento": year, "Sentimiento": sentiment}
+
+        return sentiment_counts
+
     except HTTPException:
         raise
     except KeyError:
@@ -209,7 +237,8 @@ def sentiment_analysis(year: int):
 Sistema de recomendación:  def recommendation_system( usuario : str ): Recibe un usuario y devuelve una lista con los 5 juegos
 '''
 
-# http://localhost:8000/recomendacion_juego/6210
+# LOCAL HOST: http://localhost:8000/recomendacion_juego/6210
+# REQUEST URL: https://jhonevergallegoate-pi-ml-ops-01.onrender.com/recomendacion_juego/6210
 @app.get("/recomendacion_juego/{product_id}")
 async def recomendacion_juego(product_id:int):
     try: 
@@ -231,10 +260,10 @@ async def recomendacion_juego(product_id:int):
         for chunk in pd.read_csv(df_games_path, chunksize=chunk_size):
             # Combina las etiquetas tags y genres en una sola cadena de texto
             chunk_tags_and_genres = " ".join(chunk["tags"].fillna(" ").astype(str) + " " + chunk["genres"].fillna(" ").astype(str))
-            
+
             # Aplica el vectorizador TF-IDF al lote actual de juegos y al juego de referencia
             tfidf_matrix = tfidf_vectorizer.fit_transform([target_game_tags_and_genres, chunk_tags_and_genres])
-            
+
             # Calcula la similitud entre el juego de referencia y los juegos del lote actual
             similarity_scores_batch = cosine_similarity(tfidf_matrix)
 
@@ -242,7 +271,7 @@ async def recomendacion_juego(product_id:int):
                 similarity_scores = similarity_scores_batch
             else:
                 similarity_scores = np.concatenate((similarity_scores, similarity_scores_batch), axis=1)
-                
+
         if similarity_scores is not None:
             # Obtiene los índices de los juegos similares 
             similar_games_indices = similarity_scores[0].argsort()[::-1]
@@ -250,6 +279,7 @@ async def recomendacion_juego(product_id:int):
             num_recomendation = 5
             recommended_games = df_games.iloc[similar_games_indices[1:num_recomendation + 1]]
             # Devuelve la lista con los juegos recomendados 
+
             return recommended_games[["app_name", "id"]].to_dict(orient="records")
 
         return {"message": "No se encontraron juegos similares"}
